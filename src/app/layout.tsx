77b74dd6100a1b6efe91ff0ba538b145f6268c4c
TassanuAndrew <kimtrp0311@gmail.com>
2026-04-27 09:57:29 +0700

    add pwa icons + manifest + og image2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://kanomjeen-bannongbo.vercel.app"),
   title: "บ้านขนมจีน",
-  description: "สั่งขนมจีนออนไลน์",
+  description: "สั่งขนมจีนออนไลน์ ส่งถึงบ้าน",
   manifest: "/manifest.json",
   icons: {
     icon: [
